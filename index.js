36ff237b28041c24349f4901bb60a8fe9b8adb2a
C Lewis <casserolecodes@gmail.com>
2023-08-19 16:51:39 -0400

    basic JavaScript functionality for radio button selection

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 /****** VARIABLES ******/
 
-
-
+let selectedValue;  
+let setSelectedValue;
+const radioEl = document.getElementsByName('options')
 
 
 /****** FUNCTIONS ******/
 
-
-
-
+function getRadioValue() {
+  radioEl.forEach(radio => {
+    if (radio.checked) {
+        selectedValue = radio.value;
+    }
+    })
+    return selectedValue
+}
 
 /****** EVENT LISTENERS ******/
+document.getElementById('radio-field').addEventListener('change', () => {
+    setSelectedValue = getRadioValue()
+    document.getElementById('selection').textContent = `You have chosen ${selectedValue}.`;
+        
+})
